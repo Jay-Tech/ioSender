caa2e2598b1bfe16b92edecf0e8def7e8309a444
Jason Hall <jayh160@yahoo.com>
2023-11-23 14:26:24 -0600

    updates to game pad logic

--- ioSender Touch/ioSender Touch/Utility/HandController.cs
+++ ioSender Touch/ioSender Touch/Utility/HandController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Windows.Gaming.Input;
 using CNC.Core;
+using ioSenderTouch.Controls;
 using Timer = System.Timers.Timer;
 
 namespace ioSenderTouch.Utility
@@ -18,6 +20,7 @@ namespace ioSenderTouch.Utility
         private GamepadButtons _previousDown;
         private GamepadButtons _currentDown;
         private bool _zMoving;
+        private double[] _feedRate = new []{100.0,500.0,1000.0,2000.0};
 
         public HandController(GrblViewModel grblViewModel)
         {
@@ -39,9 +42,11 @@ namespace ioSenderTouch.Utility
                 {
                       
                     case GamepadButtons.A:
-                        command="G10L20P0Z0";
+                        command = $"$J = G91G21Z{_grblViewModel.JogStep}F{_grblViewModel.JogRate}";
+                        //command="G10L20P0Z0";
                         break;
                     case GamepadButtons.B:
+                        command = $"$J = G91G21Z-{_grblViewModel.JogStep}F{_grblViewModel.JogRate}";
                         break;
                     case GamepadButtons.Y:
                        command = "G10L20P0Y0";
@@ -63,29 +68,46 @@ namespace ioSenderTouch.Utility
                     case GamepadButtons.DPadDown:
                         command=$"$J = G91G21Y-{_grblViewModel.JogStep}F{_grblViewModel.JogRate}";
                         break;
-                    
+                    case GamepadButtons.RightShoulder:
+                        
+                        if (JobFeedRate != JogFeed.Feed3)
+                        {
+                            JobFeedRate += 1;
+                            SetJogRate();
+                        }
+                        else
+                        {
+                            JobFeedRate = JogFeed.Feed0;
+                            SetJogRate();
+                        }
+                        break;
+
                 }
 
+                if (Math.Abs(input.RightTrigger - 1) < 0.1)
+                {
+                    command = "G10L20P0Z0";
+                }
+                
                
                 var x = Math.Round(input.LeftThumbstickX, 1);
                 var y = Math.Round(input.LeftThumbstickY, 1);
-                if (y > .8)
-                {
-                  command =   $"$J = G91G21Z{_grblViewModel.JogStep}F{_grblViewModel.JogRate}";
-                  _zMoving = true;
-                }
+                //if (y > .8)
+                //{
+                //  command =   $"$J = G91G21Z{_grblViewModel.JogStep}F{_grblViewModel.JogRate}";
+                //  _zMoving = true;
+                //}
                 
-
-                else if (y < -.8)
-                {
-                    command = $"$J = G91G21Z-{_grblViewModel.JogStep}F{_grblViewModel.JogRate}";
-                    _zMoving = true ;
-                }
-                else if(_zMoving)
-                {
-                    Comms.com.WriteByte(GrblConstants.CMD_JOG_CANCEL);
-                    _zMoving = false;
-                }
+                //else if (y < -.8)
+                //{
+                //    command = $"$J = G91G21Z-{_grblViewModel.JogStep}F{_grblViewModel.JogRate}";
+                //    _zMoving = true ;
+                //}
+                //else if(_zMoving)
+                //{
+                //    Comms.com.WriteByte(GrblConstants.CMD_JOG_CANCEL);
+                //    _zMoving = false;
+                //}
                
                 if (input.Buttons == GamepadButtons.None && _previousDown != GamepadButtons.None)
                 {
@@ -101,6 +123,15 @@ namespace ioSenderTouch.Utility
                 _previousDown = _currentDown;
             }
         }
+
+        private void SetJogRate()
+        {
+            _grblViewModel.JogRate = FeedRate;
+        }
+
+        public double FeedRate { get { return _feedRate[(int)JobFeedRate]; } }
+
+        public JogFeed JobFeedRate { get; set; }
 
         private void Gamepad_GamepadRemoved(object sender, Gamepad e)
         {

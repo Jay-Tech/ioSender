907eb6abd4874a954f60ce1ec2c6e88a6462aa3c
Jay-Tech <Jayh160@yahoo.com>
2024-04-07 14:50:57 -0500

    verison number update

--- ioSender Touch/ioSender Touch/MainWindow.xaml.cs
+++ ioSender Touch/ioSender Touch/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace ioSenderTouch
 {
     public partial class MainWindow : Window
     {
-        private const string Version = "1.0.1.1b";
+        private const string Version = "1.0.1.2b";
         private const string App_Name = "IO Sender Touch";
 
         private readonly GrblViewModel _viewModel;

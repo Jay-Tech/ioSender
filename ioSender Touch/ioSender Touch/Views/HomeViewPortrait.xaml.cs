f863eb8bbc1c41c279626d9c22f89fd58e4f7dfa
Jay-Tech <Jayh160@yahoo.com>
2025-02-26 16:19:31 -0600

    Ui cleanup

--- ioSender Touch/ioSender Touch/Views/HomeViewPortrait.xaml.cs
+++ ioSender Touch/ioSender Touch/Views/HomeViewPortrait.xaml.cs
@@ -1,31 +1,14 @@
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using ioSenderTouch.Controls;
-using ioSenderTouch.Controls.Probing;
-using ioSenderTouch.Controls.Render;
 using ioSenderTouch.GrblCore;
-using ioSenderTouch.Utility;
 using ioSenderTouch.ViewModels;
 
 namespace ioSenderTouch.Views
 {
     public partial class HomeViewPortrait : UserControl
     {
-        private bool? initOK = null;
-        private bool isBooted = false;
-        private IInputElement focusedControl = null;
-        private Controller _controller = null;
         private readonly GrblViewModel _model;
-        private readonly GrblConfigView _grblSettingView;
-        private readonly AppConfigView _grblAppSettings;
-        private ProbingView _probeView;
-        private readonly RenderControl _renderView;
-        private readonly OffsetView _offsetView;
-        private SDCardView _sdView;
-        private ToolView _toolView;
-        private readonly UtilityView _utilityView;
-
+        
         public HomeViewPortrait(GrblViewModel model)
         {
             _model = model;
@@ -54,19 +37,6 @@ namespace ioSenderTouch.Views
             return _model.Keyboard.ProcessKeypress(e, !(MdiControl.IsFocused || DRO.IsFocused || spindleControl.IsFocused || workParametersControl.IsFocused));
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            LeftBorder.Visibility = Visibility.Visible;
-            RightMenuBorder.Visibility = Visibility.Visible;
-            FillBorder.Child = _renderView;
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            LeftBorder.Visibility = Visibility.Collapsed;
-            RightMenuBorder.Visibility = Visibility.Collapsed;
-            FillBorder.Child = _grblAppSettings;
-        }
 
     }
 }
